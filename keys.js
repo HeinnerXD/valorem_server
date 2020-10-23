7a76795c2886a7a4f0d01001d4ae12b6a034e4ce
Takeshi <heinner45@gmail.com>
2020-10-23 17:23:22 -0500

    change in port

--- keys.js
+++ keys.js
@@ -4,7 +4,7 @@ const keys = {
     db_url: 'zeus.hana.prod.us-east-1.whitney.dbaas.ondemand.com:22398',
     db_user: 'DBADMIN',
     db_password: 'Va10r3mh2022GT',
-    port: process.env.port || 3000
+    port: process.env.PORT || 3000
 }
 
 module.exports = keys;
