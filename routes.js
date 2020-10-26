37f2391c74beec7d9698517e3dbe3d29089b3882
Takeshi <heinner45@gmail.com>
2020-10-26 16:42:07 -0500

    Change password module again

--- routes.js
+++ routes.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 const dbController = require('./controllers/db_controller');
 
 router.post('/connect', dbController.validateLogin);
-router.put('/password', dbController.changePassword);
+router.post('/password', dbController.changePassword);
 
 module.exports = router;
