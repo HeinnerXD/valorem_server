54828aa999bc7b62311a2378f876cd00c0d81026
Takeshi <heinner45@gmail.com>
2020-11-04 17:20:31 -0500

    try with another package

--- routes.js
+++ routes.js
@@ -2,22 +2,26 @@
 const router = require('express').Router();
 const dbController = require('./controllers/db_controller');
 const sharepointController = require('./controllers/sharepoint_controller');
-const fs = require('fs')
+const fs = require('fs');
+var rimraf = require("rimraf");
 
 router.post('/connect', dbController.validateLogin);
 router.post('/password', dbController.changePassword);
-router.post('/downloadFile', sharepointController.downloadFile );
-router.post('/cleanServer', (req, res)=>{
-    const dir = './valoremFiles';
-    fs.rmdir(dir, { recursive: true }, (err) => {
-        if (err) {
-            throw err
-        }
-        console.log(`${dir} is deleted!`);
-        return res.status(200).send({response: `${dir} is deleted!`})
+router.post('/downloadFile', sharepointController.downloadFile);
+router.post('/cleanServer', (req, res) => {
+    rimraf("./valoremFiles", function () { 
+        console.log("done"); 
+        res.status(200).send({response: "Directory deleted"})
     });
 });
- 
+
 module.exports = router;
 
-//change
+// const dir = './valoremFiles';
+//     fs.rmdir(dir, { recursive: true }, (err) => {
+//         if (err) {
+//             throw err
+//         }
+//         console.log(`${dir} is deleted!`);
+//         return res.status(200).send({response: `${dir} is deleted!`})
+//     });
